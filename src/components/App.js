c2f68db6f5227ce941fadad9a1da25145be301eb
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-19 22:23:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,23 @@ import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here 
-  
-
-  
+  const [button1,setButton1]=useState("ON")
+  const [button2,setButton2]=useState("OFF")
+  const handleClick=()=>{
+    button1==="ON"? setButton1("OFF") : setButton1("ON")
+    button1==="ON"? setButton2("ON") : setButton2("OFF")
+    
+  }
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+      {/* assign value for button 1 */button1}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+        {/* assign value for button 2 */button2}
       </button>
     </div>
   );
 }
-
-
-export default App;
